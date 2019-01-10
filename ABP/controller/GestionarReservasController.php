18031a91be7128b2fb8295fc44bfc881f46c04cf
Nacho <rodriguezpereiranacho@gmail.com>
2019-01-10 02:38:27 +0100

    Promocionar Partido

--- ABP/controller/GestionarReservasController.php
+++ ABP/controller/GestionarReservasController.php
@@ -1,90 +1,95 @@
 <?php
+
 //file: controller/EnfrentamientoController.php
 
+require_once(__DIR__ . "/../model/Partido.php");
+require_once(__DIR__ . "/../model/PartidoMapper.php");
+require_once(__DIR__ . "/../model/PartidoReserva.php");
+require_once(__DIR__ . "/../model/PartidoReservaMapper.php");
+require_once(__DIR__ . "/../model/GestionarReservas.php");
+require_once(__DIR__ . "/../model/GestionarReservasMapper.php");
+require_once(__DIR__ . "/../core/ViewManager.php");
+require_once(__DIR__ . "/../controller/BaseController.php");
 
-require_once(__DIR__."/../model/GestionarReservas.php");
-require_once(__DIR__."/../model/GestionarReservasMapper.php");
-require_once(__DIR__."/../core/ViewManager.php");
-require_once(__DIR__."/../controller/BaseController.php");
 /**
-* @author Victor
-*/
+ * @author Victor
+ */
 class GestionarReservasController extends BaseController {
 
-	private $gestionarReservasMapper;
+    private $gestionarReservasMapper;
+    private $partidoMapper;
+    private $partidoReservaMapper;
 
-	public function __construct() {
-		parent::__construct();
-		$this->GestionarReservasMapper = new GestionarReservasMapper();
+    public function __construct() {
+        parent::__construct();
+        $this->GestionarReservasMapper = new GestionarReservasMapper();
+        $this->partidoMapper = new PartidoMapper();
+        $this->partidoReservaMapper = new PartidoReservaMapper();
+    }
 
+    public function index() {
+        $this->view->render("reservas", "gestionarReservas");
+    }
 
-	}
-	public function index() {
-			$this->view->render("reservas", "gestionarReservas");
+    public function verPistasFecha() {
 
-	}
-	public function verPistasFecha() {
+        if (isset($_REQUEST['fecha'])) {
+            $dia = $_REQUEST["fecha"];
+            $fecha = $this->GestionarReservasMapper->findByFecha($dia);
+            $this->view->setVariable("fecha", $fecha, false);
+            $this->view->render("reservas", "gestionarReservas");
+        }
+    }
 
-		if (isset($_REQUEST['fecha'])){
-			$dia=$_REQUEST["fecha"];
-			$fecha= $this->GestionarReservasMapper->findByFecha($dia);
-			$this->view->setVariable("fecha", $fecha, false);
-			$this->view->render("reservas", "gestionarReservas");
-		}
-	}
+    public function verPistasFechaPartido() {
+
+        if (isset($_REQUEST['fecha'])) {
+            $dia = $_REQUEST["fecha"];
+            $fecha = $this->GestionarReservasMapper->findByFecha($dia);
+            $this->view->setVariable("fecha", $fecha, false);
+            $this->view->render("partido", "index");
+        }
+    }
+
+    public function comprobarReserva() {
+        // populate the Post object with data form the form
+        $pista = $_REQUEST["pista"];
+
+        $hora = $_REQUEST["hora"];
+        $fecha = $_REQUEST["fecha"];
+
+        $disponibilidad = $_REQUEST["disponibilidad"];
+        $minutosAumentar = 90;
+        $segundos_horaInicio = strtotime($hora);
+        $segundos_minutoAumentar = $minutosAumentar * 60;
+        $horaFinal = date("H:i", $segundos_horaInicio + $segundos_minutoAumentar);
+        $this->view->setVariable("pista", $pista, false);
+        $this->view->setVariable("hora", $hora, false);
+        $this->view->setVariable("fecha", $fecha, false);
+        $this->view->setVariable("disponibilidad", $disponibilidad, false);
+        $this->view->setVariable("horaFinal", $horaFinal, false);
+
+        $this->view->render("reservas", "reserva");
+    }
+
+    public function comprobarReservaPartido() {
+
+        $pista = $_REQUEST["pista"];
+        $hora = $_REQUEST["hora"];
+        $fecha = $_REQUEST["fecha"];
+        $disponibilidad = $_REQUEST["disponibilidad"];
+        $minutosAumentar = 90;
+        $segundos_horaInicio = strtotime($hora);
+        $segundos_minutoAumentar = $minutosAumentar * 60;
+        $horaFinal = date("H:i", $segundos_horaInicio + $segundos_minutoAumentar);       
         
-        public function verPistasFechaPartido() {
+        $this->view->setVariable("pista", $pista, false);
+        $this->view->setVariable("hora", $hora, false);
+        $this->view->setVariable("fecha", $fecha, false);
+        $this->view->setVariable("disponibilidad", $disponibilidad, false);
+        $this->view->setVariable("horaFinal", $horaFinal, false);
 
-		if (isset($_REQUEST['fecha'])){
-			$dia=$_REQUEST["fecha"];
-			$fecha= $this->GestionarReservasMapper->findByFecha($dia);
-			$this->view->setVariable("fecha", $fecha, false);
-			$this->view->render("partido", "index");
-		}
-	}
-        
-	public function comprobarReserva() {
-			// populate the Post object with data form the form
-			$pista=$_REQUEST["pista"];
-
-			$hora=$_REQUEST["hora"];
-			$fecha=$_REQUEST["fecha"];
-
-			$disponibilidad=$_REQUEST["disponibilidad"];
-			$minutosAumentar=90;
-			$segundos_horaInicio=strtotime($hora);
-			$segundos_minutoAumentar = $minutosAumentar*60;
-			$horaFinal=date("H:i",$segundos_horaInicio+$segundos_minutoAumentar);
-			$this->view->setVariable("pista", $pista, false);
-			$this->view->setVariable("hora", $hora, false);
-			$this->view->setVariable("fecha", $fecha, false);
-			$this->view->setVariable("disponibilidad", $disponibilidad, false);
-			$this->view->setVariable("horaFinal", $horaFinal, false);
-
-			$this->view->render("reservas", "reserva");
-
-	}
-        
-        public function comprobarReservaPartido() {
-			
-			$pista = $_REQUEST["pista"];
-			$hora = $_REQUEST["hora"];
-			$fecha = $_REQUEST["fecha"];
-			$disponibilidad=$_REQUEST["disponibilidad"];
-			$minutosAumentar=90;
-			$segundos_horaInicio=strtotime($hora);
-			$segundos_minutoAumentar = $minutosAumentar*60;
-			$horaFinal=date("H:i",$segundos_horaInicio+$segundos_minutoAumentar);                        
-
-                        $this->view->setVariable("pista", $pista, false);
-			$this->view->setVariable("hora", $hora, false);
-			$this->view->setVariable("fecha", $fecha, false);
-			$this->view->setVariable("disponibilidad", $disponibilidad, false);
-			$this->view->setVariable("horaFinal", $horaFinal, false);
-
-			$this->view->render("partido", "reserva");
-
-	}
-        
+        $this->view->render("partido", "reserva");
+    }
 
 }
--- ABP/controller/RealizarReservaController.php
+++ ABP/controller/RealizarReservaController.php
@@ -7,6 +7,8 @@ require_once(__DIR__."/../core/ViewManager.php");
 require_once(__DIR__."/../controller/BaseController.php");
 require_once(__DIR__ . "/../model/Partido.php");
 require_once(__DIR__ . "/../model/PartidoMapper.php");
+require_once(__DIR__ . "/../model/PartidoReserva.php");
+require_once(__DIR__ . "/../model/PartidoReservaMapper.php");
 /**
 * @author Victor
 */
@@ -15,79 +17,92 @@ class RealizarReservaController extends BaseController {
 
 	private $RealizarReservaController;
         private $partidoMapper;
+        private $partidoReservaMapper;
 
 	public function __construct() {
 		parent::__construct();
 		$this->RealizarReservaMapper = new RealizarReservaMapper();
 		$this->GestionarReservasMapper = new GestionarReservasMapper();
                 $this->partidoMapper = new PartidoMapper();
+                $this->partidoReservaMapper = new PartidoReservaMapper();
 	}
 	public function index() {
 		$this->view->render("reservas", "gestionarReservas");
 
 	}
 
+    public function anadirReserva() {
+        $reserva = new RealizarReserva();
+        // populate the Post object with data form the form
+        $pista = $_REQUEST["pista"];
+        $hora = $_REQUEST["hora"];
+        $horaFin = $_REQUEST["horaFinal"];
+        $fecha = $_REQUEST["fecha"];
+        $disponibilidad = $_REQUEST["disponibilidad"];
+        $reserva->setPistaIdPista($pista);
+        $reserva->setHoraInicio($hora);
+        $reserva->setHoraFin($horaFin);
+        $reserva->setFecha($fecha);
 
-	public function anadirReserva() {
-			$reserva = new RealizarReserva();
-			// populate the Post object with data form the form
-			$pista=$_REQUEST["pista"];
-			$hora=$_REQUEST["hora"];
-			$horaFin=$_REQUEST["horaFinal"];
-			$fecha=$_REQUEST["fecha"];
-			$disponibilidad=$_REQUEST["disponibilidad"];
-			$reserva->setPistaIdPista($pista);
-			$reserva->setHoraInicio($hora);
-			$reserva->setHoraFin($horaFin);
-			$reserva->setFecha($fecha);
+        if ($disponibilidad == 'disponible') {
+            $disponibilidad = 'ocupado';
+            $reserva->setDisponibilidad('ocupado');
+            $this->RealizarReservaMapper->insertarReserva($reserva);
+            $this->view->setFlash("Operación realizada");
+            $this->GestionarReservasMapper->updateHorario($disponibilidad, $pista, $hora, $fecha);
+        } else {
+            $disponibilidad = 'disponible';
+            $reserva->setDisponibilidad('disponible');
+            $this->RealizarReservaMapper->cancelarReserva($reserva);
+            $this->view->setFlash("Operación realizada");
+            $this->GestionarReservasMapper->updateHorario($disponibilidad, $pista, $hora, $fecha);
+        }
 
-			if($disponibilidad=='disponible'){
-				$disponibilidad='ocupado';
-				$reserva->setDisponibilidad('ocupado');
-				$this->RealizarReservaMapper->insertarReserva($reserva);
-				$this->view->setFlash("Operación realizada");
-				$this->GestionarReservasMapper->updateHorario($disponibilidad,$pista,$hora,$fecha);
-			}else{
-				$disponibilidad='disponible';
-				$reserva->setDisponibilidad('disponible');
-				$this->RealizarReservaMapper->cancelarReserva($reserva);
-				$this->view->setFlash("Operación realizada");
-				$this->GestionarReservasMapper->updateHorario($disponibilidad,$pista,$hora,$fecha);
-			}
+        $this->view->render("reservas", "gestionarReservas");
+    }
 
-			$this->view->render("reservas", "gestionarReservas");
+    public function anadirReservaPartido() {
+        $reserva = new RealizarReserva();
 
-	}
-        
-        public function anadirReservaPartido() {
-			$reserva = new RealizarReserva();
-			
-			$pista=$_REQUEST["pista"];
-			$hora=$_REQUEST["hora"];
-			$horaFin=$_REQUEST["horaFinal"];
-			$fecha=$_REQUEST["fecha"];
-			$disponibilidad=$_REQUEST["disponibilidad"];
-			$reserva->setPistaIdPista($pista);
-			$reserva->setHoraInicio($hora);
-			$reserva->setHoraFin($horaFin);
-			$reserva->setFecha($fecha);
+        $pista = $_REQUEST["pista"];
+        $hora = $_REQUEST["hora"];
+        $horaFin = $_REQUEST["horaFinal"];
+        $fecha = $_REQUEST["fecha"];
+        $disponibilidad = $_REQUEST["disponibilidad"];
+        $reserva->setPistaIdPista($pista);
+        $reserva->setHoraInicio($hora);
+        $reserva->setHoraFin($horaFin);
+        $reserva->setFecha($fecha);
 
-			if($disponibilidad=='disponible'){
-				$disponibilidad='ocupado';
-				$reserva->setDisponibilidad('ocupado');                               
-				$this->RealizarReservaMapper->insertarReserva($reserva);                               				
-				$this->GestionarReservasMapper->updateHorario($disponibilidad,$pista,$hora,$fecha);
-			}else{
-				$disponibilidad='disponible';
-				$reserva->setDisponibilidad('disponible');
-				$this->RealizarReservaMapper->cancelarReserva($reserva);                                			
-				$this->GestionarReservasMapper->updateHorario($disponibilidad,$pista,$hora,$fecha);
-			}
+        if ($disponibilidad == 'disponible') {
+            $disponibilidad = 'ocupado';
+            $reserva->setDisponibilidad('ocupado');
+            
+            $partido = new Partido();
+            $partido->setFecha($fecha);
+            $partido->setHoraInicio($hora);
+            $partido->setHoraFin($horaFin);
+            $idPartido = $this->partidoMapper->save($partido);
 
-			$partidos = $this->partidoMapper->findAll();
-                        $this->view->setVariable("partidos", $partidos, false);
-                        $this->view->render("partido", "index");
+            $idReserva = $this->RealizarReservaMapper->insertarReserva($reserva);
 
-	}
+            $partidoReserva = new PartidoReserva();
+            $partidoReserva->setIdPartido($idPartido);
+            $partidoReserva->setIdReserva($idReserva);
+            $this->partidoReservaMapper->save($partidoReserva);
+            
+            $this->GestionarReservasMapper->updateHorario($disponibilidad, $pista, $hora, $fecha);
+            
+        } else {
+            $disponibilidad = 'disponible';
+            $reserva->setDisponibilidad('disponible');
+            $this->RealizarReservaMapper->cancelarReserva($reserva);
+            $this->GestionarReservasMapper->updateHorario($disponibilidad, $pista, $hora, $fecha);
+        }
+
+        $partidos = $this->partidoMapper->findAll();
+        $this->view->setVariable("partidos", $partidos, false);
+        $this->view->render("partido", "index");
+    }
 
 }
